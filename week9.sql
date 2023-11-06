2c3aa09dca63afb32e063b44fcf99f2ac0792b7f
ifotn <rich@infrontofthenet.com>
2023-11-06 13:33:33 -0500

    l9/2 - group by

--- week9.sql
+++ week9.sql
@@ -40,3 +40,56 @@ VALUES ('Adam', 'Clark', 'History', '2002-05-27');
 -- USE DATEDIFF() to calculate all students ages and update all records
 UPDATE students SET age = ROUND(DATEDIFF('2023-11-06', dateOfBirth)/365, 0)
 WHERE studentId < 27;
+
+-- GROUPING PRACTICE
+-- average price of all donuts
+SELECT AVG(price) FROM donuts;
+
+-- average price for each type of donut
+SELECT AVG(price), type FROM donuts GROUP BY type;
+
+-- avg price per type sorted a-z
+SELECT AVG(price), type FROM donuts GROUP BY type ORDER BY AVG(price);
+
+-- can we move ORDER BY before GROUP BY?  NOPE!  GROUP BY MUST COME FIRST!
+-- SELECT AVG(price), type FROM donuts ORDER BY AVG(price) GROUP BY type;
+
+-- what is the lowest rating?
+SELECT MIN(rating) FROM donuts;
+
+-- what is the highest rating?
+SELECT MAX(rating) FROM donuts;
+
+-- how many donuts have no rating?
+SELECT COUNT(name) FROM donuts WHERE rating IS NULL;
+
+-- how many students in each major?
+SELECT COUNT(major), major FROM students GROUP BY major;
+
+-- How many orders are there for each delivery option?
+SELECT deliveryOption, COUNT(orderId) FROM orders GROUP BY deliveryOption;
+
+-- What is the total revenue for each payment type?
+SELECT SUM(orderTotal), paymentType FROM orders GROUP BY paymentType;
+
+-- the single biggest order?
+SELECT MAX(orderTotal), orderId FROM orders GROUP BY orderId LIMIT 1;
+
+-- subquery to show both aggregate and individual data
+SELECT * FROM orders
+WHERE orderTotal = (SELECT MAX(orderTotal) FROM orders); 
+
+-- alternative: use ORDER BY DESC + LIMIT:
+SELECT * FROM orders ORDER BY orderTotal DESC LIMIT 1;
+
+-- What’s the average cost of an order for each payment type
+SELECT AVG(orderTotal), paymentType FROM orders GROUP BY paymentType;
+
+-- What’s the average age of all students?
+SELECT AVG(age) FROM students;
+
+-- What’s the average age of all students in each department?
+SELECT major, AVG(age) FROM students GROUP BY major ORDER BY AVG(age) DESC, major;
+
+-- Who is the youngest student?
+SELECT * FROM students ORDER BY dateOfBirth DESC LIMIT 1;
