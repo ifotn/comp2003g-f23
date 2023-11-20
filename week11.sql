f393593630c8763d239daa7f239e243c64a82d40
ifotn <Rich.Freeman@GeorgianCollege.ca>
2023-11-20 13:39:15 -0500

    l11/2 - completed join scripts

--- week11.sql
+++ week11.sql
@@ -12,6 +12,11 @@ DELETE FROM beers WHERE beerId > 8;
 SELECT * FROM sales;
 DELETE FROM sales WHERE salesId > 12;
 
+SELECT * FROM beerCases;
+DELETE FROM beerCases WHERE beerCaseId > 21;
+SELECT * FROM cases;
+DELETE FROM cases WHERE caseId > 3;
+
 -- now show Brewer / Beer pairings with a JOIN
 SELECT brewers.*, beers.*
 FROM brewers 
@@ -66,6 +71,42 @@ LEFT OUTER JOIN sales ON beerCases.beerCaseId = sales.beerCaseId
 GROUP BY beers.name
 ORDER by saleCount DESC;
 
+ -- Show all beers made by Labatt
+SELECT brewers.name AS brewer, beers.name AS beer
+FROM brewers
+INNER JOIN beers ON brewers.brewerId = beers.brewerId
+WHERE brewers.name = 'Labatt';
+
+-- Show all beers available in 24 packs
+SELECT beers.name AS beer, cases.size AS size
+FROM beers
+INNER JOIN beerCases ON beers.beerId = beerCases.beerId
+INNER JOIN cases ON beerCases.caseId = cases.caseId
+WHERE cases.size = 24;
+
+-- Show the total sales revenue for Molson (use a Group function)
+SELECT SUM(sales.price) AS revenue, brewers.name AS brewer
+FROM sales
+INNER JOIN beerCases ON sales.beerCaseId = beerCases.beerCaseId
+INNER JOIN beers ON beerCases.beerId = beers.beerId
+INNER JOIN brewers ON beers.brewerId = brewers.brewerId
+WHERE brewers.name = "Molson";
+
+-- Show how many sales were made by each brewer (use a Group function)
+SELECT COUNT(salesId) AS sales, brewers.name
+FROM sales
+INNER JOIN beerCases ON sales.beerCaseId = beerCases.beerCaseId
+INNER JOIN beers ON beerCases.beerId = beers.beerId
+RIGHT OUTER JOIN brewers ON beers.brewerId = brewers.brewerId
+GROUP BY brewers.name;
+
+-- Show the total sales revenue by case size (use a Group function)
+SELECT SUM(sales.price*sales.quantity) AS revenue, cases.size AS size
+FROM sales
+INNER JOIN beerCases ON sales.beerCaseId = beerCases.beerCaseId
+INNER JOIN cases ON beerCases.caseId = cases.caseId
+GROUP BY size
+ORDER BY revenue DESC;
 
 
 
